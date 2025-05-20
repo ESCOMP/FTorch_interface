6d03d7c46b7adeca12e7bb5e7995e23386d2251c
Jim Edwards <jedwards@ucar.edu>
2025-05-20 11:04:31 -0600

    add support for torch_delete

--- FTorch_cesm_interface.F90
+++ FTorch_cesm_interface.F90
@@ -1,10 +1,10 @@
 module FTorch_cesm_interface
 #ifdef USE_FTORCH
   use ftorch,         only: torch_kCPU, torch_tensor, torch_model, torch_tensor_from_array
-  use ftorch,         only: torch_model_load, torch_model_forward
+  use ftorch,         only: torch_model_load, torch_model_forward, torch_delete
 #else
   use shr_abort_mod, only : shr_abort_abort
-  use shr_kind_mod, only : r8 => shr_kind_r8
+  use shr_kind_mod, only : r4 => shr_kind_r4
   type :: torch_model
   end type torch_model
   type :: torch_tensor
@@ -20,7 +20,7 @@ contains
   end subroutine torch_model_load
   subroutine torch_tensor_from_array(tensor, data, layout, i)
     type(torch_tensor) :: tensor
-    real(r8) :: data(:,:)
+    real(r4) :: data(:,:)
     integer :: layout(:)
     integer :: i
     call shr_abort_abort(message)
@@ -30,6 +30,9 @@ contains
     type(torch_tensor) :: tensor_in(:), tensor_out(:)
     call shr_abort_abort(message)
   end subroutine torch_model_forward
-
+  subroutine torch_delete(tensor)
+    type(torch_tensor) :: tensor
+    call shr_abort_abort(message)
+  end subroutine torch_delete
 #endif
 end module FTorch_cesm_interface
